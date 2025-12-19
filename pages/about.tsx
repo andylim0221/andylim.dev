c5c126e17c51f494906d1ba4e8dad6ca6817e491
andylim0221 <limchinhuat0221@gmail.com>
2025-12-19 14:26:36 +0800

    chore: update resume link in About page

--- pages/about.tsx
+++ pages/about.tsx
@@ -17,7 +17,7 @@ export default function About() {
             className="bg-white dark:bg-gray-500 dark:hover:bg-gray-700 dark:text-white hover:bg-gray-200 text-gray-800 font-semibold py-2 px-4 border border-black dark:border-white rounded shadow cursor-pointer"
             onClick={ () => {
               window.open(
-                `${RESUME_URL ?? 'https://d3fdk6ilfp4n6v.cloudfront.net/public/LimChinHuat_Resume.pdf'}`, 
+                `${RESUME_URL ?? 'https://d3fdk6ilfp4n6v.cloudfront.net/public/AndyLim_Resume.pdf'}`, 
                 '_blank', 
                 'noopener'
               )
